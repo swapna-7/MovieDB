e259b8ae38b1e91b4e65f24cca1297aa714c3f7c
VELA-ANNAMALAI <vela.a0067@gmail.com>
2024-05-22 19:48:20 +0530

    config files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ClerkProvider from './provider/CheckProvide.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ClerkProvider>
     <App />
+
+    </ClerkProvider>
+
+
   </React.StrictMode>,
 )
+
